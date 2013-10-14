5afa16b3c59d3b1510f9f9c207915da26165b127
mewlist <mewlist@mewlist.com>
2013-10-14 22:02:27 +0900

    ようこそページをつくった

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def navbar_link_to name, opts, html_opts = nil
+    html_opts ||= {}
+    html_opts[:class] ||= ""
+    if url_for(opts) == url_for(params)
+      html_opts[:class] += " active"
+    end
+    content_tag :li, link_to( name, opts ), html_opts
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ MewlistCom::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'welcome#index'
+  get '/works' => 'welcome#works'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
