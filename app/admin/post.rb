383ff2d092f8c072854ed38d94c2727aadde7d5a
mewlist <mewlist@mewlist.com>
2013-10-20 01:38:25 +0900

    Preview markdown in admin console

--- app/admin/post.rb
+++ app/admin/post.rb
@@ -1,7 +1,15 @@
 ActiveAdmin.register Post do
   controller do
+    def show
+      @markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, :autolink => true, :space_after_headers => true)
+    end
+
     def permitted_params
       params.permit post: [:title, :body]
     end
+  end
+
+  show do |post|
+    render "blog/post", post: post
   end
 end
