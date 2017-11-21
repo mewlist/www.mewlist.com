01d3c9c1c869bd8c4f3df1872fcb173173f364e3
mewlist <mewlist@mewlist.com>
2017-11-21 09:42:00 +0900

    Initialize

--- config/initializers/session_store.rb
+++
@@ -1,3 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-MewlistCom::Application.config.session_store :cookie_store, key: '_mewlist_com_session'
--- db/migrate/20131019163939_add_published_column_to_post.rb
+++
@@ -1,5 +0,0 @@
-class AddPublishedColumnToPost < ActiveRecord::Migration
-  def change
-    add_column :posts, :published, :boolean
-  end
-end
